e9d35ee4becafbba2b56e3e8cb11c0d198400505
Beatriz Vieira <55674401+vbribeiro@users.noreply.github.com>
2024-05-21 01:05:34 +0000

    ajustando analises

--- F4-DATA-VIZ-PRODUCTION-MODELS/app.py
+++ F4-DATA-VIZ-PRODUCTION-MODELS/app.py
@@ -15,7 +15,6 @@ def load_dataset():
         "https://raw.githubusercontent.com/icarocarmona/Tech-Challenges-DTAT-Grupo-26/main/F4-DATA-VIZ-PRODUCTION-MODELS/petroleo_bruto.csv")
     return df
 
-
 # Inicio do visual
 
 st.set_page_config(
@@ -28,7 +27,6 @@ st.set_page_config(
 st.header('⛽ Tech Challege - Grupo 26', divider='rainbow')
 
 df = load_dataset()
-
 
 def plot_predict(current_week_dates, current_week_prices, next_week_dates, next_week_predictions):
     # Criar as figuras
@@ -79,13 +77,11 @@ def plot_predict(current_week_dates, current_week_prices, next_week_dates, next_
 #   fig.show()
     st.plotly_chart(fig)
 
-
 y = df['Preco'].values  # Output é o preço atual
 
 model = joblib.load('./modelo.pkl')
 
 # st.write(model.predict(y[-1].reshape(1, -1)))
-
 
 # Fazer previsões para a próxima semana usando os últimos dados conhecidos
 last_known_data = y[-1].reshape(1, -1)
@@ -110,7 +106,7 @@ fig = px.line(data_frame=df, x='Data', y='Preco')
 
 st.plotly_chart(fig, use_container_width=True)
 
-st.dataframe(df)
+# st.dataframe(df)
 
 st.write("# Previsões")
 plot_predict(current_week_dates, current_week_prices,
@@ -129,11 +125,7 @@ dfp['Ano'] = dfp['Data'].dt.year
 # Agrupando os dados por ano e calculando a média dos preços
 preco_anual = dfp.groupby('Ano')['Preco'].mean()
 
-# Criando o gráfico de linha
-fig = go.Figure()
-
-# fig.add_trace(go.Scatter(
-# INÍCIO BIA - Variações Anuais do Preço do Petróleo Brent
+#Variações Anuais do Preço do Petróleo Brent
 
 st.write("# Variações Anuais do Preço do Petróleo Brent")
 
@@ -146,30 +138,6 @@ df['Ano'] = df['Data'].dt.year
 # Agrupando os dados por ano e calculando a média dos preços
 preco_anual = df.groupby('Ano')['Preco'].mean()
 
-# Agrupando os dados por ano e calculando a média dos preços (Gráfico Base da Pri)
-preco_anual = df.groupby('Ano')['Preco'].mean()
-
-# Criando o gráfico de linha
-anual = go.Figure()
-
-anual.add_trace(go.Scatter(
-    x=preco_anual.index,
-    y=preco_anual.values,
-    mode='lines+markers',
-    name='Preço Médio Anual'
-))
-
-# Adicionando título e rótulos
-fig.update_layout(
-    title='Variações Anuais do Preço do Petróleo Brent',
-    xaxis_title='Ano',
-    yaxis_title='Preço Médio',
-    xaxis=dict(tickmode='linear')
-)
-
-# Exibir o gráfico no Streamlit
-st.plotly_chart(fig)
-
 # Criando o gráfico de barras
 fig = go.Figure()
 
@@ -181,17 +149,8 @@ fig.add_trace(go.Bar(
     textangle=45  # Inclinação do texto
 ))
 
-# Adicionando título e rótulos
-anual.update_layout(
-    title='Variações registradas no DataFrame',
-    xaxis_title='Ano',
-    yaxis_title='Preço Médio',
-    xaxis=dict(tickmode='linear')
-)
-
 # Exibir o gráfico no Streamlit
 st.plotly_chart(fig)
-
 
 # Convertendo a coluna 'Data' para o tipo datetime
 dfp['Data'] = pd.to_datetime(dfp['Data'])
@@ -251,7 +210,6 @@ Em 2015, o preço do barril de petróleo Brent fechou a 31 de dezembro a 37,10 d
 
 st.write(texto, unsafe_allow_html=True)
 
-
 # Atribuir o dataframe a uma nova variável dfp
 dfp = df
 
@@ -306,74 +264,9 @@ dfp['Ano'] = dfp['Data'].dt.year
 # Calcular a média do preço do petróleo para cada ano
 media_por_ano = dfp.groupby('Ano').mean()['Preco']
 
-# Criando o gráfico de linha
-fig = go.Figure()
-
-fig.add_trace(go.Scatter(
-    x=media_por_ano.index,
-    y=media_por_ano.values,
-    mode='lines+markers',
-    line=dict(color='orange'),
-    marker=dict(symbol='circle', size=8),
-    name='Preço Médio Anual'
-))
-
-# Configurando o layout
-fig.update_layout(
-    title='Média do Preço do Petróleo por Ano',
-    xaxis_title='Ano',
-    yaxis_title='Preço em Dólar',
-    xaxis=dict(tickmode='linear'),
-    yaxis=dict(tickformat=".2f")
-)
-
-# Exibir o gráfico no Streamlit
-st.plotly_chart(fig)
-
-# Convertendo a coluna 'Data' para o tipo datetime
-dfp.set_index('Data', inplace=True)
-
-# Decompor a série temporal
-decomposicao = seasonal_decompose(dfp['Preco'], model='additive', period=12)
-
-# Criar figuras individuais para cada componente da decomposição
-fig_observed = go.Scatter(
-    x=dfp.index, y=decomposicao.observed, mode='lines', name='Observado')
-fig_trend = go.Scatter(x=dfp.index, y=decomposicao.trend,
-                       mode='lines', name='Tendência')
-fig_seasonal = go.Scatter(x=dfp.index, y=decomposicao.seasonal,
-                          mode='lines', name='Sazonalidade')
-fig_residual = go.Scatter(x=dfp.index, y=decomposicao.resid,
-                          mode='lines', name='Resíduo')
-
-# Criar o subplot
-fig = go.Figure()
-
-fig.add_trace(fig_observed)
-fig.add_trace(fig_trend)
-fig.add_trace(fig_seasonal)
-fig.add_trace(fig_residual)
-
-# Configurar o layout
-fig.update_layout(
-    title='Decomposição da Série Temporal',
-    xaxis_title='Data',
-    yaxis_title='Preço',
-    xaxis=dict(tickmode='auto', tickformat='%Y-%m')
-)
-
-# Exibir o gráfico no Streamlit
-st.plotly_chart(fig)
-
-st.plotly_chart(anual, use_container_width=True)
-
-st.write("Correlação preço do petróleo com a taxa de câmbio: https://www.ibp.org.br/observatorio-do-setor/snapshots/preco-do-petroleo-e-taxa-de-cambio/")
-
 # ANO 2016
 
 st.write("# Análise ano 2016")
-
-st.write(dfp.head())
 
 # Pegando ano de 2016
 dados_2016 = dfp[dfp['Ano'] == 2016]
@@ -387,8 +280,8 @@ media_mensal_2016 = dados_2016.groupby('Mes')['Preco'].mean()
 ano2016 = go.Figure()
 
 ano2016.add_trace(go.Scatter(
-    x=media_mensal_2016.index,
-    y=dados_2016['Preco'].values,
+    x=dados_2016['Preco'].index,
+    y=media_mensal_2016.values,
     mode='lines+markers',
     name='Média 2016'
 ))
@@ -403,7 +296,7 @@ ano2016.update_layout(
 
 st.plotly_chart(ano2016, use_container_width=True)
 
-# JUSTIFICATIVA st.write("## Anuário estatístico: https://www.gov.br/anp/pt-br/centrais-de-conteudo/publicacoes/anuario-estatistico/anuario-estatistico-2016 --/-- Petróleo abaixo de 50 dólares: https://g1.globo.com/economia/mercados/noticia/2016/10/petroleo-segue-abaixo-de-us-50-ha-mais-de-1-ano-veja-impactos.html")
+st.write("Anuário estatístico: https://www.gov.br/anp/pt-br/centrais-de-conteudo/publicacoes/anuario-estatistico/anuario-estatistico-2016 --/-- Petróleo abaixo de 50 dólares: https://g1.globo.com/economia/mercados/noticia/2016/10/petroleo-segue-abaixo-de-us-50-ha-mais-de-1-ano-veja-impactos.html")
 
 # ANO 2020
 
@@ -475,4 +368,41 @@ st.plotly_chart(ano2023, use_container_width=True)
 
 # JUSTIFICATIVA st.write("## Geral GOV: https://www.gov.br/anp/pt-br/canais_atendimento/imprensa/noticias-comunicados/reservas-provadas-de-petroleo-no-brasil-crescem-7-em-2023#:~:text=Em%202023%2C%20houve%20aumento%20de,prov%C3%A1veis%20e%20poss%C3%ADveis%20(3P). --/-- Highlights: https://agenciabrasil.ebc.com.br/economia/noticia/2024-02/producao-media-de-petroleo-e-gas-bate-recorde-em-2023-informa-anp)
 
-# FIM BIA
+# Convertendo a coluna 'Data' para o tipo datetime
+dfp.set_index('Data', inplace=True)
+
+# Decompor a série temporal
+decomposicao = seasonal_decompose(dfp['Preco'], model='additive', period=12)
+
+# Criar figuras individuais para cada componente da decomposição
+fig_observed = go.Scatter(
+    x=dfp.index, y=decomposicao.observed, mode='lines', name='Observado')
+fig_trend = go.Scatter(x=dfp.index, y=decomposicao.trend,
+                       mode='lines', name='Tendência')
+fig_seasonal = go.Scatter(x=dfp.index, y=decomposicao.seasonal,
+                          mode='lines', name='Sazonalidade')
+fig_residual = go.Scatter(x=dfp.index, y=decomposicao.resid,
+                          mode='lines', name='Resíduo')
+
+# Criar o subplot
+fig = go.Figure()
+
+fig.add_trace(fig_observed)
+fig.add_trace(fig_trend)
+fig.add_trace(fig_seasonal)
+fig.add_trace(fig_residual)
+
+# Configurar o layout
+fig.update_layout(
+    title='Decomposição da Série Temporal',
+    xaxis_title='Data',
+    yaxis_title='Preço',
+    xaxis=dict(tickmode='auto', tickformat='%Y-%m')
+)
+
+# Exibir o gráfico no Streamlit
+st.plotly_chart(fig)
+
+st.write("Utilizamos a decomposição de séries temporais para entender o que compõem a série e como eles contribuem para suas variações ao longo do tempo. Ela foi útil para identificarmos padrões e tendências em dados temporais. Remover efeitos sazonais para análise de tendências de longo prazo." 
+         
+"Avaliar a eficácia de modelos de previsão e detecção de anomalias e apoiar a tomada de decisões. O gráfico foi valioso para entender a estrutura e o comportamento de dados temporais.")
